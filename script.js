be4e8644bb5cb92f424e492717a765b1cc5bfa01
Anuj Maurya <anuj24122001@gmail.com>
2025-03-09 00:53:07 +0530

    finalise
    Now with better UI and mobile responsive.

--- script.js
+++ script.js
@@ -1,90 +1,142 @@
-let title ="";
- 
-document.getElementById("search-btn").addEventListener("click",searchMovie);
-function searchMovie() {
-    const searchTerm = document.getElementById("input-box").value;
+let searchResult = document.getElementById("search-result");
 
-    
+document.getElementById("search-btn").addEventListener("click", searchMovie);
+
+function searchMovie() {
+    const searchTerm = document.getElementById("input-box").value.trim();
     if (searchTerm === "") {
         alert("Please enter a movie name before searching!"); // Alert if empty
         return; // Stop function execution
     }
+    clearScreenAndShowLoader();
+    fetchMoviesBySearchTerm(searchTerm)
+        .then(movies => {
+            // console.log(movies);
+            if (movies.length === 0) return;
+            const validMovies = checkUndefinedObjects(movies);
+            const uniqueMovies = removeDuplicateMovies(validMovies);
+            uniqueMovies.forEach(movie => fetchMovieById(movie.imdbID))
+        });
+};
 
-    // console.log(searchTerm);
-    document.getElementById('search-result').innerHTML = "";
-    document.getElementById("input-box").value = "";
-    fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=d99b99e6`)
+function fetchMoviesBySearchTerm(searchTerm) {
+    return fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=d99b99e6`)
         .then(res => res.json())
         .then(data => {
-            // console.log(data)
-
             if (data.Response === "False") {
-                // console.log(data.Error)
-                document.getElementById("search-result").innerHTML = `<h2 id ="fetching-error"> ${data.Error} </h2>`
-                return;
+                searchResult.innerHTML = `<h2 id ="fetching-error"> ${data.Error} </h2>`
+                clearLoader();
+                return [];
             }
-            data.Search.forEach(movie => { 
-                // console.log(movie.Poster)
-                title = movie.Title
-                // console.log(title);
-                fetch(`https://www.omdbapi.com/?t=${title}&apikey=d99b99e6`)
-                    .then(response => response.json())
-                    .then(movieData => {
-                        console.log(movieData)
-                    displayMovie(movieData);
-                     });
-                    // showPoster(movie)
-            });     
-        });
+            return data.Search;
+        })
 }
- 
+
+function fetchMovieById(movieId) {
+    fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=d99b99e6`)
+        .then(response => response.json())
+        .then(movieData => {
+            if (movieData.Response === "True") {
+                displayMovie(movieData);
+                clearLoader();
+            }
+        })
+        .catch(error => console.error("Error fetching movie details:", error));
+}
+
+function removeDuplicateMovies(movies) {
+    let uniqueMovies = [];
+    for (let i = 0; i < movies.length; i++) {
+        let isDuplicate = false;
+
+        for (let j = 0; j < uniqueMovies.length; j++) {
+            if (movies[i].imdbID === uniqueMovies[j].imdbID) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            uniqueMovies.push(movies[i]);
+        }
+    }
+    return uniqueMovies;
+}
+
+function checkUndefinedObjects(movies) {
+    return movies.filter(movie => movie.Response !== "False");
+}
 
 function displayMovie(movie) {
     const movieElement = document.createElement('div');
     movieElement.className = "movie-display";
     movieElement.innerHTML = ` 
-    <div class ="movie"> 
+        <div class ="movie"> 
             <img src = ${movie.Poster} class = "image" /> 
-                <div class ="textInfo"> 
-                    <div id ="titleData" > <h3 id= "titleOfMovie" > ${movie.Title} </h3> 
-                        <p> <img src = ${"star.png"} class = "imageStar" /> ${movie.imdbRating} </p>
-                    </div>
-                     
-                    <div class ="data1">  
-                            <p>${movie.Runtime} &nbsp;&nbsp; ${movie.Genre} &nbsp;&nbsp; ${movie.Type} </p> 
-        
-                            <button class ="watchlist-btn" id = "add-btn"> <img src = ${"addIcon.png"} class = "image1" /> Watchlist </button>
-                    </div>
-                    <p>${movie.Plot}</p>
-                    <p>${movie.Actors}</p>
-                    
-                </div> 
-    </div> 
-    <div class="hr-container">
-     <hr />
-    </div>   `;
-    document.getElementById('search-result').appendChild(movieElement);
+            <div class ="textInfo"> 
+                <div id ="titleData" > <h3 id= "titleOfMovie" > ${movie.Title} </h3> 
+                            <p> <img src = ${"star.png"} class = "imageStar" /> ${movie.imdbRating} </p>
+                </div>
+ 
+                <div class ="data1">
+                    <p>${movie.Runtime} &nbsp;&nbsp; ${movie.Genre} &nbsp;&nbsp; ${movie.Type} </p>            
+                    <button class ="watchlist-btn" id = "add-btn"> 
+                        <img src = ${"addIcon.png"} class = "addRemoveIcon" /> Watchlist 
+                    </button>
+                </div>
+                <p>${movie.Plot}</p>
+                <p>${movie.Actors}</p>      
+            </div> 
+        </div> 
+        <div class="hr-container">
+        <hr />
+        </div>   
+    `;
 
-        let watchlistBtn = movieElement.querySelector(".watchlist-btn");
-        watchlistBtn.addEventListener("click", function () {
-           // alert(`${movie.Title} added to watchlist!`)
-            //localStorage.setItem("my-watchlist")
-            let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
-            let movieExists = watchlist.some(item => item.imdbID === movie.imdbID);
-            if (movieExists) {
-                alert(`${movie.Title} is already in your watchlist!`);
-                return;
-            }
-                watchlist.push(movie);
-            localStorage.setItem("watchlist", JSON.stringify(watchlist));
-            alert(`${movie.Title} added to watchlist`);
-            
-            // console.log(JSON.parse(localStorage.getItem("watchlist")))
-            // console.log(`${movie.Title} added to watchlist!`);
-            
-        });
+    searchResult.appendChild(movieElement);
+
+    let watchlistBtn = movieElement.querySelector(".watchlist-btn");
+    watchlistBtn.addEventListener("click", function () {
+        addToWatchlist(movie);
+    });
 }
 
-   
- 
- 
+function addToWatchlist(movie) {
+    let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+    let movieExists = watchlist.some(item => item.imdbID === movie.imdbID);
+    if (movieExists) {
+        alert(`${movie.Title} is already in your Watchlist!`)
+        return;
+    }
+    watchlist.push(movie);
+    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    alert(`${movie.Title} added to your Watchlist.`)
+}
+
+function clearScreenAndShowLoader() {
+    const backgroundText = document.getElementById("background-text");
+    const loadingText = document.getElementById("loader");
+
+    if (backgroundText) {
+        backgroundText.style.display = "none";
+    }
+
+    if (searchResult) {
+        searchResult.innerHTML = "";
+    }
+
+    if (loadingText) {
+        loadingText.style.display = "block";
+    }
+
+    const inputBox = document.getElementById("input-box");
+    if (inputBox) {
+        inputBox.value = "";
+    }
+}
+
+function clearLoader() {
+    const loadingText = document.getElementById("loader"); // Define loadingText properly
+    if (loadingText) {
+        loadingText.style.display = "none";
+    }
+}
--- watchlist.js
+++ watchlist.js
@@ -1,40 +1,95 @@
-function loadWatchlist(){
-    const watchList = JSON.parse(localStorage.getItem("watchlist")) || [];
-    const watchlistContainer = document.getElementById("search-result")
-
-    if(watchList.length === 0){
-        watchlistContainer.innerHTML = 
-        '<div id ="watchlist-backkground-text"> <h1> No movies in your watchlist.</h1> <div>  <a id ="go-back-link" href="index.html"> <img src = "addIcon.png" class = "image1" /> Add some movies</a></div> </div>';
+function createMovieElements(movieArray) {
+    const watchlistContainer = document.getElementById("search-result");
+    
+    const emptyWatchlistMessage = `
+        <div id="watchlist-background-text"> 
+            <h1> No movies in your watchlist.</h1> 
+            <div>  
+                <a id="go-back-link" href="index.html"> 
+                    <img src="addIcon.png" class="addRemoveIcon" /> Add some movies
+                </a>
+            </div>
+        </div>
+    `;
+    if (movieArray.length === 0) {
+       watchlistContainer.innerHTML =  emptyWatchlistMessage;
         return;
     }
 
-    //create a function pass array 
-    // that function iteratively creates div and pushes each object 
-    
-    watchlistContainer.innerHTML = watchList.map((movie,index)  =>  ` 
-    <div class ="movie"> 
-            <img src = ${movie.Poster} class = "image" /> 
-                <div class ="textInfo"> 
-                     <div id ="titleData" > <h3 id= "titleOfMovie" > ${movie.Title} </h3> 
-                        <p> <img src = ${"star.png"} class = "imageStar" /> ${movie.imdbRating} </p>
-                    </div> 
-                        <div class ="data1">  
-                            <p>${movie.Runtime} &nbsp;&nbsp; ${movie.Genre} &nbsp;&nbsp; ${movie.Type} </p> 
-        
-                            <button class ="watchlist-btn" id = "add-btn" onclick="removeFromWatchlist(${index})"> <img src = ${"addIcon.png"} class = "image1" /> Remove </button>
-                        </div>
-                    <p>${movie.Plot}</p>
-                    <p>${movie.Actors}</p>
-                    
-                </div> 
-    </div> 
-    <div class="hr-container">
-     <hr />
-    </div>  `).join("");
+    watchlistContainer.innerHTML = '';
+
+    // Iteratively create and append divs for each movie
+    movieArray.forEach((movie, index) => {
+        // Create main movie div
+        const movieDiv = document.createElement('div');
+        movieDiv.className = 'movie';
+
+        // Create image element
+        const img = document.createElement('img');
+        img.src = movie.Poster;
+        img.className = 'image';
+
+        // Create text info container
+        const textInfoDiv = document.createElement('div');
+        textInfoDiv.className = 'textInfo';
+
+        // Title and rating section
+        const titleDataDiv = document.createElement('div');
+        titleDataDiv.id = 'titleData';
+        titleDataDiv.innerHTML = `
+            <h3 id="titleOfMovie">${movie.Title}</h3>
+            <p><img src="star.png" class="imageStar" />${movie.imdbRating}</p>
+        `;
+
+        // Movie details and button section
+        const dataDiv = document.createElement('div');
+        dataDiv.className = 'data1';
+        const button = document.createElement('button');
+        button.className = 'watchlist-btn';
+        button.id = 'add-btn';
+        button.innerHTML = `<img src="remove.png" class="addRemoveIcon" /> Remove`;
+        button.onclick = () => removeFromWatchlist(index);
+
+        dataDiv.innerHTML = `
+            <p>${movie.Runtime} &nbsp;&nbsp; ${movie.Genre} &nbsp;&nbsp; ${movie.Type}</p>
+        `;
+        dataDiv.appendChild(button);
+
+        // Add plot and actors
+        const plotP = document.createElement('p');
+        plotP.textContent = movie.Plot;
+        const actorsP = document.createElement('p');
+        actorsP.textContent = movie.Actors;
+
+        // Assemble text info
+        textInfoDiv.appendChild(titleDataDiv);
+        textInfoDiv.appendChild(dataDiv);
+        textInfoDiv.appendChild(plotP);
+        textInfoDiv.appendChild(actorsP);
+
+        // Assemble movie div
+        movieDiv.appendChild(img);
+        movieDiv.appendChild(textInfoDiv);
+
+        // Create and add hr container
+        const hrContainer = document.createElement('div');
+        hrContainer.className = 'hr-container';
+        hrContainer.innerHTML = '<hr />';
+
+        // Add to container
+        watchlistContainer.appendChild(movieDiv);
+        watchlistContainer.appendChild(hrContainer);
+    });
 }
+
+function loadWatchlist() {
+    const watchList = JSON.parse(localStorage.getItem("watchlist")) || [];
+    createMovieElements(watchList);
+}
+
 function removeFromWatchlist(index) {
     let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
-    watchlist.splice(index, 1); 
+    watchlist.splice(index, 1);
     localStorage.setItem("watchlist", JSON.stringify(watchlist));
     loadWatchlist();
 }
